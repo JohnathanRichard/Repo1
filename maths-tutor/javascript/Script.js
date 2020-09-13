a91831a635afa386aa79a4c229251dcd757aaae7
Johnathan Richard <anrd8028@gmail.com.com>
2020-09-13 22:16:20 +0530

    working JavaScript validation form

--- maths-tutor/javascript/Script.js
+++ maths-tutor/javascript/Script.js
@@ -1,18 +1,21 @@
-function ValidateEmail(inputText)
+function ValidateEmail(currObjThatIClicked)
 {
 
-    
-var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-if(inputText.value.match(mailformat))
-{
-alert("Valid email address!");
-document.form1.text1.focus();
-return true;
-}
-else
-{
-alert("You have entered an invalid email address!");
-document.form1.text1.focus();
-return false;
-}
+    console.log(currObjThatIClicked.type +" : "+ currObjThatIClicked.id);
+
+
+     var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if(currObjThatIClicked.value.match(mailformat))
+    {
+        document.getElementById('error-display-id').innerHTML = 'valid email address!';
+    document.form1.currObjThatIClicked;
+    return true;
+    }
+    else
+    {
+    document.getElementById('error-display-id').innerHTML = ' invalid email address!';
+
+    document.form1.email1.focus();
+    return false;
+    }
 }
