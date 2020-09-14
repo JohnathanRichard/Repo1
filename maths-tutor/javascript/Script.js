a14c572872fb57d96ee07f13ce8f6ecf81814045
Johnathan Richard <anrd8028@gmail.com.com>
2020-09-14 17:43:46 +0530

    Working email detection

--- maths-tutor/javascript/Script.js
+++ maths-tutor/javascript/Script.js
@@ -7,15 +7,29 @@ function ValidateEmail(currObjThatIClicked)
      var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
     if(currObjThatIClicked.value.match(mailformat))
     {
-        document.getElementById('error-display-id').innerHTML = 'valid email address!';
+        document.getElementById('error-display-id').innerHTML = 'Valid email address!';
     document.form1.currObjThatIClicked;
     return true;
     }
     else
     {
-    document.getElementById('error-display-id').innerHTML = ' invalid email address!';
+    document.getElementById('error-display-id').innerHTML = ' Invalid email address!';
 
     document.form1.email1.focus();
     return false;
     }
 }
+
+
+
+
+var input = document.getElementsByName('email1');
+
+  input.addEventListener('Keydown',function (event) {
+    console.log(event.keyCode);
+  
+    if (event.keyCode === 13 );{
+     event.preventDefault();
+     document.getElementById("btn 11").click();
+    }
+  });
