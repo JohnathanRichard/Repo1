a0375db338bb583711c8d7707431c63759be2308
Johnathan Richard <anrd8028@gmail.com.com>
2020-09-17 12:31:44 +0530

    adding comments

--- maths-tutor/javascript/Script.js
+++ maths-tutor/javascript/Script.js
@@ -1,35 +1,31 @@
-function ValidateEmail(currObjThatIClicked)
-{
+// ! Working don't change !!!
 
-    console.log(currObjThatIClicked.type +" : "+ currObjThatIClicked.id);
+function ValidateEmail(currObjThatIClicked) {
+  console.log(currObjThatIClicked.type + " : " + currObjThatIClicked.id);
 
-
-     var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-    if(currObjThatIClicked.value.match(mailformat))
-    {
-        document.getElementById('error-display-id').innerHTML = 'Valid email address!';
+  var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  if (currObjThatIClicked.value.match(mailformat)) {
+    document.getElementById("error-display-id").innerHTML =
+      "Valid email address!";
     document.form1.currObjThatIClicked;
     return true;
-    }
-    else
-    {
-    document.getElementById('error-display-id').innerHTML = ' Invalid email address!';
+  } else {
+    document.getElementById("error-display-id").innerHTML =
+      " Invalid email address!";
 
     document.form1.email1.focus();
     return false;
-    }
+  }
 }
 
+var input = document.getElementsByName("email1");
 
+input.addEventListener("Keydown", function (event) {
+  console.log(event.keyCode);
 
-
-var input = document.getElementsByName('email1');
-
-  input.addEventListener('Keydown',function (event) {
-    console.log(event.keyCode);
-  
-    if (event.keyCode === 13 );{
-     event.preventDefault();
-     document.getElementById("btn 11").click();
-    }
-  });
+  if (event.keyCode === 13);
+  {
+    event.preventDefault();
+    document.getElementById("btn 11").click();
+  }
+});
